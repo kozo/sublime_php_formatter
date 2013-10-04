f0e854dfad2e08fb6ed63e562b7422091e69296c
saku <kozo.sakuragawa@gmail.com>
2013-10-04 10:24:04 +0900

    自動保存しないように修正

--- php_formatter.py
+++ php_formatter.py
@@ -3,9 +3,12 @@
 
 import sublime, sublime_plugin
 import os
+import sys
 
 class PhpFormatterCommand(sublime_plugin.TextCommand):
     def run(self, edit):
+        reload(sys)
+        sys.setdefaultencoding("utf-8")
         # projectパスの取り方はこれでいいのか？
         #projectDir = self.view.window().folders()[0]
 
@@ -20,11 +23,21 @@ class PhpFormatterCommand(sublime_plugin.TextCommand):
         #phpCBPath = pluginPath + "hoge.bat"
         # 半角スペースを含むのでダブルコーテーションで囲む
         phpCBPath = '"' + phpCBPath + '"'
-
         phpCBPath = phpCBPath + " " + self.view.file_name()
         print(phpCBPath)
         os.system(phpCBPath)
 
+        # 1次ファイルパス
+        tempFilePath = tempPath + "\\phpcb.php"
+        source = open(tempFilePath).read()
+
+        regionAll = sublime.Region(0, self.view.size())
+        self.view.replace(edit, regionAll, source)
+        # self.view.replace(edit, regionAll, source)
+
         # sublime.packages_path() + "User\php_formater.bat"
         #print(sublime.packages_path())
+
+        # 再描画
+        # self._force_refresh()
 
